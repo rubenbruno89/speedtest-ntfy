98bb571ae205af30ec7f92f4cd527300fedaadc4
Ruben Bruno Cavalcante de Menezes <34634888+rubenbruno89@users.noreply.github.com>
2026-08-16 03:02:43 -0300

    Fix spacing in output file command

--- speedtest-ntfy.bat
+++ speedtest-ntfy.bat
@@ -4,6 +4,6 @@ echo IP Publico
 curl "ifconfig.me"
 echo.
 echo Salvando o resultado no arquivo velocidade.txt
-speedtest >velocidade.txt
+speedtest > velocidade.txt
 curl --upload-file velocidade.txt ntfy.sh/Alarmenoc
 exit
